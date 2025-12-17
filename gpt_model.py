fa6b701dc6088b9e237326c3c4dca3ad46f42ae4
radburt44 <radcliffeburt123@gmail.com>
2025-12-16 18:50:52 -0600

    notes/comments

--- gpt_model.py
+++ gpt_model.py
@@ -6,8 +6,8 @@ GPT_CONFIG_124M = {
     "vocab_size": 50257,    # Vocabulary size
     "context_length": 1024, # Context length
     "emb_dim": 768,         # Embedding dimension
-    "n_heads": 12,          # Number of attention heads
-    "n_layers": 12,         # Number of layers
+    "n_heads": 12,          # Number of attention heads within each block 
+    "n_layers": 12,         # Number of layers or how many transformer blocks
     "drop_rate": 0.1,       # Dropout rate
     "qkv_bias": False       # Query-Key-Value bias
 }
